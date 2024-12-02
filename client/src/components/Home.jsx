30f0f658de2a751c6f09f5b359708892cb379626
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-02 22:53:18 +0530

    animation add

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import milk from "../assets/milk.png";
+import bag from "../assets/bag.png";
 import "../App.css";
 const Home = () => {
   return (
@@ -10,8 +10,8 @@ const Home = () => {
         <header className="w-full flex justify-between items-center px-8 py-4 bg-orange-500">
           {/* Logo Section */}
           <div className="flex items-center space-x-2">
-            <img src={logo} alt="Logo" className="h-8" />
-            <h1 className="text-xl font-bold">SubhamStore</h1>
+            <img id="logo-container" src={logo} alt="Logo" className="h-8" />
+            <h1 className="text-xl font-bold">Shubham Store</h1>
           </div>
           {/* Links Section */}
           <div className="flex items-center text-white space-x-6">
@@ -28,12 +28,12 @@ const Home = () => {
         </header>
 
         {/* Main Section */}
-        <main className="flex flex-col items-center font-gilroy text-center px-8 flex-grow">
+        <main className="flex flex-col mt-16 items-center font-gilroy text-center px-8 flex-grow">
           <h1 className="text-3xl md:text-5xl font-bold mt-8">
             Order Dairy products & groceries. Daily services!
           </h1>
-          <div className="content-start">
-            <img src={milk} alt="milk" />
+          <div id="image-container" className="content-start">
+            <img src={bag} alt="bag" />
           </div>
 
           {/* Cards Section */}
@@ -42,25 +42,25 @@ const Home = () => {
             className=" flex flex-wrap justify-center items-stretch gap-6 mt-12 w-full max-w-5xl"
           >
             {/* Card 1 */}
-            <div className="bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
+            <div className="card-container1 bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
               <h2 className="text-xl font-bold">FOOD DELIVERY</h2>
               <p className="mt-2">FROM RESTAURANTS</p>
               <span className="text-orange-500 font-bold">UPTO 60% OFF</span>
             </div>
             {/* Card 2 */}
-            <div className="bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
+            <div className="card-container2 bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
               <h2 className="text-xl font-bold">INSTAMART</h2>
               <p className="mt-2">INSTANT GROCERY</p>
               <span className="text-orange-500 font-bold">UPTO 60% OFF</span>
             </div>
             {/* Card 3 */}
-            <div className="bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
+            <div className="card-container3 bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
               <h2 className="text-xl font-bold">DINEOUT</h2>
               <p className="mt-2">EAT OUT & SAVE MORE</p>
               <span className="text-orange-500 font-bold">UPTO 50% OFF</span>
             </div>
             {/* Card 4 */}
-            <div className="bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
+            <div className="card-container4 bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm">
               <h2 className="text-xl font-bold">GENIE</h2>
               <p className="mt-2">PICK-UP & DROP</p>
               <span className="text-orange-500 font-bold">UPTO 50% OFF</span>
