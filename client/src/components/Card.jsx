ca79bd579955e73f084f591c22665856eb19f125
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-12 19:03:30 +0530

    image card updated

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,10 +4,12 @@ import { bestSellProducts } from "./Utils";
 
 function CardItem({ header, description, price }) {
   return (
-    <div className="card-container  bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm duration-500 ease-in-out transform hover:rotate-3 hover:scale-105">
-      <h2 className="text-xl font-bold">{header}</h2>
-      <p className="mt-2">{description}</p>
-      <span className="text-orange-500 font-bold">{price}</span>
+    <div className="card-container bg-white text-black p-6 rounded-lg shadow-lg flex-1 max-w-xs md:max-w-sm min-h-[150px] duration-500 backdrop-brightness-200 ease-in-out transform hover:rotate-3 hover:scale-105 hover:bg-[url('assets/image1.webp')] bg-cover cursor-pointer relative overflow-hidden ">
+      <div className="absolute inset-0 bg-white opacity-100 hover:opacity-0 duration-500 ease-in-out flex flex-col justify-center items-center p-6">
+        <h2 className="text-xl font-bold">{header}</h2>
+        <p className="mt-2">{description}</p>
+        <span className="text-orange-500 font-bold">{price}</span>
+      </div>
     </div>
   );
 }
@@ -16,7 +18,7 @@ function Card() {
   return (
     <div
       id="card-container"
-      className="flex flex-wrap justify-center items-stretch  gap-6 mt-32 w-3/5 max-w-6xl px-4 duration-1000"
+      className="flex flex-wrap  justify-center items-stretch  gap-6 mt-32 w-3/5 max-w-6xl px-4 duration-1000 "
     >
       {bestSellProducts.map((product, index) => (
         <CardItem
--- client/src/components/Utils.jsx
+++ client/src/components/Utils.jsx
@@ -1,25 +1,5 @@
 export const bestSellProducts = [
-  { header: "Milk", dic: "good quality", price: "20 Rupee" },
-  { header: "Milk", dic: "good quality", price: "20 Rupee" },
-  { header: "Milk", dic: "good quality", price: "20 Rupee" },
-  { header: "Milk", dic: "good quality", price: "20 Rupee" },
-];
-
-export const product = [
-  {
-    brandName: "Aachi",
-    brandImage: "../assets/productImages/Aachi/webp/Aachi.webp",
-    productImage: [
-      "../assets/productImages/Aachi/webp/Aachi Biryani Masala - 100g.webp",
-      "../assets/productImages/Aachi/webp/Aachi Chicken Kabab , 65 Masala.webp",
-    ],
-  },
-  {
-    brandName: "Aavin",
-    brandImage: "../assets/productImages/aavin/webp/aavin.webp",
-    productImage: [
-      "../assets/productImages/Aachi/webp/Aachi Biryani Masala - 100g.webp",
-      "../assets/productImages/Aachi/webp/Aachi Chicken Kabab , 65 Masala.webp",
-    ],
-  },
+  { header: "Milk Products", dic: "purity and nourishment" },
+  { header: "Groceries ", dic: "Top brands and High quality" },
+  { header: "Spices ", dic: "flavors and ignite memories" },
 ];
