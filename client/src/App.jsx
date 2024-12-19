f6e89a43c4e5a0f780fd05346afc6d4703e85c43
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-12-19 20:38:39 +0530

    Servies added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import Home from "./components/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Brands from "./components/Brands";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Home />
       <Brands />
+      <Services />
     </>
   );
 }
---
+++ client/src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import services from "../assets/Frame 1.png";
+function Services() {
+  return (
+    <div>
+      <h1 className="text-black text-lg sm:text-4xl mt-8 font-serif sm:mx-24">
+        How Delivery Work!
+      </h1>
+      <img src={services} alt="How Delivery Work!" />
+    </div>
+  );
+}
+
+export default Services;
